7da821c4a1d978bba89bd7aebc0eecc7f2ed8d19
Adam Lyon <lyon@fnal.gov>
2020-09-02 21:50:24 -0500

    Switch to docker image

--- project/setup.sh
+++ project/setup.sh
@@ -6,9 +6,9 @@ setup gm2 v9_52_00 -q prof
 setup cmake v3_17_1
 setup gdb
 export USER=lyon
-export STUDIO_PROJECT_PATH=/Users/lyon/Development/gm2/irmaData/project
-export STUDIO_PROJECT_BUILD_PATH=/Users/lyon/Development/gm2/irmaData/project/build
-export STUDIO_PROJECT_SRC_PATH=/Users/lyon/Development/gm2/irmaData/project/src
-[[ -z "${FHICL_FILE_PATH}" ]] && FHICL_FILE_PATH=/Users/lyon/Development/gm2/irmaData/project/src || FHICL_FILE_PATH=/Users/lyon/Development/gm2/irmaData/project/src:$FHICL_FILE_PATH
+export STUDIO_PROJECT_PATH=/workspaces/irmaData/project
+export STUDIO_PROJECT_BUILD_PATH=/workspaces/irmaData/project/build
+export STUDIO_PROJECT_SRC_PATH=/workspaces/irmaData/project/src
+[[ -z "${FHICL_FILE_PATH}" ]] && FHICL_FILE_PATH=/workspaces/irmaData/project/src || FHICL_FILE_PATH=/workspaces/irmaData/project/src:$FHICL_FILE_PATH
 export FHICL_FILE_PATH
-LD_LIBRARY_PATH=/Users/lyon/Development/gm2/irmaData/project/build/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=/workspaces/irmaData/project/build/lib:$LD_LIBRARY_PATH
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-source ${LOCAL_WORKSPACE_FOLDER}/project/setup.sh
+source ${WORKSPACE_FOLDER}/project/setup.sh
 
 #env > ${STUDIO_PROJECT_PATH}/env.out
 #ups active > ${STUDIO_PROJECT_PATH}/active.out
