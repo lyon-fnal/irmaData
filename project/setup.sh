bb610812e50a7906891702ccd39d8e6f12907edf
Adam Lyon <lyon@fnal.gov>
2020-09-02 22:14:54 -0500

    Use WORKSPACE_FOLDER instead of hard path

--- project/setup.sh
+++ project/setup.sh
@@ -5,10 +5,9 @@ setup studio
 setup gm2 v9_52_00 -q prof
 setup cmake v3_17_1
 setup gdb
-export USER=lyon
-export STUDIO_PROJECT_PATH=/workspaces/irmaData/project
-export STUDIO_PROJECT_BUILD_PATH=/workspaces/irmaData/project/build
-export STUDIO_PROJECT_SRC_PATH=/workspaces/irmaData/project/src
-[[ -z "${FHICL_FILE_PATH}" ]] && FHICL_FILE_PATH=/workspaces/irmaData/project/src || FHICL_FILE_PATH=/workspaces/irmaData/project/src:$FHICL_FILE_PATH
+export STUDIO_PROJECT_PATH=${WORKSPACE_FOLDER}/project
+export STUDIO_PROJECT_BUILD_PATH=${WORKSPACE_FOLDER}/project/build
+export STUDIO_PROJECT_SRC_PATH=${WORKSPACE_FOLDER}/project/src
+[[ -z "${FHICL_FILE_PATH}" ]] && FHICL_FILE_PATH=${WORKSPACE_FOLDER}/project/src || FHICL_FILE_PATH=${WORKSPACE_FOLDER}/project/src:$FHICL_FILE_PATH
 export FHICL_FILE_PATH
-LD_LIBRARY_PATH=/workspaces/irmaData/project/build/lib:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=${WORKSPACE_FOLDER}/project/build/lib:$LD_LIBRARY_PATH
