aabfd0fdd5dd9fdce9dbac660f1e3ea285be3cae
Adam Lyon <lyon@fnal.gov>
2020-09-09 18:15:40 -0500

    hdf5 and hep_hpc are now in CVMFS

--- project/setup.sh
+++ project/setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source /cvmfs/gm2.opensciencegrid.org/prod/g-2/setup
-export PRODUCTS=${WORKSPACE_FOLDER}/project/products:/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/g-2:/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/external:$PRODUCTS  # Should go away 
-setup studio
+export PRODUCTS=/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/g-2:/cvmfs/gm2.opensciencegrid.org/specials/sl7/prod/external:$PRODUCTS  # Should go away 
 setup gm2 v9_52_00 -q prof
 setup cmake v3_17_1
 setup hep_hpc v0_13_01 -q e15
 setup gdb
+setup studio
 export STUDIO_PROJECT_PATH=${WORKSPACE_FOLDER}/project
 export STUDIO_PROJECT_BUILD_PATH=${WORKSPACE_FOLDER}/project/build
 export STUDIO_PROJECT_SRC_PATH=${WORKSPACE_FOLDER}/project/src
